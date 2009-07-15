eb55a5d70d0c87d6c66b9d4a9e9d1701c75c5c57
cvs <devnull@localhost>
2009-07-15 00:28:03 +0000

    Added membership login plugin

---
+++ plugins/MemberLogin.php
@@ -0,0 +1,72 @@
+<?
+class MemberLogin extends PluginBase
+{
+	function GetTitle()
+	{
+		return 'Member Login Plugin';
+	}
+
+	function GetUninstallSQL()
+	{
+		return '';
+	}
+
+	function GetDefaultConfig()
+	{
+		return array();
+	}
+
+	function GetCommandMenuItems()
+	{
+		$r = array();
+
+		//$this->BuildSettingsMenuItem($r);
+		$this->BuildDeleteMenuItem($r);
+
+		return $r;
+	}
+
+	function GetConfigItems()
+	{
+		$configItems = array();
+		/*$configItems[] = array(
+			"name" => 'Show as pop-up when page loads?',
+			"default" => 'No',
+			"inputspec" => 'drop.Yes,No',
+			"authlevel" => 0);
+		$configItems[] = array(
+			"name" => 'Embed Code',
+			"default" => '',
+			"inputspec" => 'textarea.60.10',
+			"authlevel" => 0);*/
+		return $configItems;
+	}
+
+	function Initialize()
+	{
+	}
+
+	function Render()
+	{
+		$r = Zymurgy::memberlogin();
+		if (!array_key_exists('reg',$_GET))
+			$r .= "<a href=\"{$_SERVER['REQUEST_URI']}?reg=username\">Not a member?  Sign-up.</a>";
+		return $r;
+	}
+
+	function AdminMenuText()
+	{
+		return '';
+	}
+
+	function RenderAdmin()
+	{
+		echo "This plugin has no settings.";
+	}
+}
+
+function MemberLoginFactory()
+{
+	return new MemberLogin();
+}
+?>
