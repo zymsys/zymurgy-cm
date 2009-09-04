7d2d7054cbe0e7ee58c087acf7fd0789189645d7
cvs <devnull@localhost>
2009-09-04 18:24:48 +0000

    *** empty log message ***

--- templates/simple.php
+++ templates/simple.php
@@ -3,6 +3,7 @@
  * WARNING: This file will be over-written when Zymurgy:CM is upgraded.  Copy it to a location outside of
  * the /zymurgy folder to make customizations to it.
  */
+ob_start();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
