fc829642f6b10b6531b86040df164c28db9c0e22
cvs <devnull@localhost>
2009-10-21 18:26:22 +0000

    Added util script to create a new group for each user in the system, if one does not already exist.

---
+++ installer/createusergroups.php
@@ -0,0 +1,30 @@
+<?php
+	include("../cmo.php");
+
+	$memberProvider = Zymurgy::initializemembership();
+
+	$sql = "SELECT `id`, `username` FROM `zcm_member`";
+	$ri = Zymurgy::$db->query($sql)
+		or die("Could not retrieve list of users: ".Zymurgy::$db->error().", $sql");
+
+	while(($row = Zymurgy::$db->fetch_array($ri)) !== FALSE)
+	{
+		echo($row["username"]."<br>");
+
+		$sql = "SELECT `id` FROM `zcm_groups` WHERE `name` = '".
+			Zymurgy::$db->escape_string($row["username"]).
+			"'";
+		$groupID = Zymurgy::$db->get($sql);
+
+		if($groupID > 0)
+		{
+			echo("-- Group for user found.<br>");
+		}
+		else
+		{
+			echo("-- Group for user not found. Creating.<br>");
+		}
+	}
+
+	Zymurgy::$db->free_result($ri);
+?>
