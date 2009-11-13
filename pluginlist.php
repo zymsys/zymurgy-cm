996f4b1723354de8486f43c006e6a7cf98abb74e
cvs <devnull@localhost>
2009-11-13 19:26:30 +0000

    Added ability to add a plugin from the local filesystem.

--- pluginlist.php
+++ pluginlist.php
@@ -1,4 +1,5 @@
 <?php
+	ini_set("display_errors", 1);
 	require_once("cmo.php");
 
 	if($_SERVER['REQUEST_METHOD'] == "POST")
@@ -8,6 +9,23 @@
 			case "act_enable":
 				EnablePlugin($_POST["id"]);
 				Zymurgy::JSRedirect("plugin.php");
+
+			case "act_add":
+				$error = ValidateFileUpload($_FILES["file"]);
+
+				if(strlen($error) > 0)
+				{
+					$breadcrumbTrail = "<a href=\"plugin.php\">Plugin Management</a> &gt; Add/Enable Plugin";
+					$ri = GetDisabledPlugins();
+					DisplayPluginList($ri, "addlist", $error);
+					Zymurgy::$db->free_result($ri);
+				}
+				else
+				{
+					InstallPlugin($_FILES["file"]);
+				}
+				Zymurgy::JSRedirect("plugin.php");
+				break;
 
 			case "act_disable":
 				DisablePlugin($_POST["id"]);
@@ -135,7 +153,133 @@
 			or die("Could not remove plugin: ".Zymurgy::$db->error().", $sql");
 	}
 
-	function DisplayPluginList($ri, $action)
+	function ValidateFileUpload($file)
+	{
+		switch ($file['error'])
+		{
+		    case UPLOAD_ERR_OK:
+		   		return "";
+		        break;
+		    case UPLOAD_ERR_INI_SIZE:
+		        return "The uploaded file exceeds the upload_max_filesize directive (".ini_get("upload_max_filesize").") in php.ini.";
+		       break;
+
+		    case UPLOAD_ERR_FORM_SIZE:
+		       return "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.";
+		       break;
+
+		    case UPLOAD_ERR_PARTIAL:
+		       return "The uploaded file was only partially uploaded.";
+		       break;
+
+		    case UPLOAD_ERR_NO_FILE:
+		       return "No file was uploaded.";
+		       break;
+
+		    case UPLOAD_ERR_NO_TMP_DIR:
+		       return "Missing a temporary folder.";
+		       break;
+		    case UPLOAD_ERR_CANT_WRITE:
+		       return "Failed to write file to disk";
+		       break;
+
+		    default:
+		       return "Unknown File Error";
+		}
+	}
+
+	function InstallPlugin($file)
+	{
+		echo("InstallPlugin START<br>");
+
+		if(!move_uploaded_file($file["tmp_name"], Zymurgy::$root."/zymurgy/plugins/temp.php"))
+		{
+			echo("Could not move file ".$file["tmp_name"]."<br>");
+		}
+		else
+		{
+			echo("File moved to temp.php<br>");
+
+			$pluginName = $file["name"];
+			$className = str_replace(".php", "", $pluginName);
+
+			include_once("PluginBase.php");
+			include_once("plugins/temp.php");
+
+			echo("Instantiating ".$pluginName);
+
+			$plugin = new $className;
+
+			if($plugin instanceof PluginBase)
+			{
+				rename("plugins/temp.php", "plugins/".$pluginName);
+				ExecuteAdd($className);
+			}
+		}
+
+		echo("InstallPlugin END<br>");
+	}
+	function  ExecuteAdd($source)
+	{
+		global $plugins;
+
+		//Get an instance of the plugin class
+		echo("---- Getting instance of $source plugin<br>");
+		$factory = "{$source}Factory";
+		$plugin = $factory();
+
+		//Create a configuration group for this plugin
+		Zymurgy::$db->run("insert into zcm_pluginconfiggroup (name) values ('".
+			Zymurgy::$db->escape_string($plugin->GetTitle()).": Default')");
+		$pcg = Zymurgy::$db->insert_id();
+
+		//Add plugin to the plugin table
+		echo("---- Adding plugin definition to database<br>");
+		Zymurgy::$db->query("insert into zcm_plugin(title,name,uninstallsql,enabled,defaultconfig) values ('".
+			Zymurgy::$db->escape_string($plugin->GetTitle())."','".
+			Zymurgy::$db->escape_string($source)."','".
+			Zymurgy::$db->escape_string($plugin->GetUninstallSQL())."',1,$pcg)");
+		$id = Zymurgy::$db->insert_id();
+		//	$id = 7;
+
+		//Add default configuration
+		echo("---- Retrieving default plugin configuration<br>");
+		$defconf = $plugin->GetDefaultConfig();
+
+		//	print_r($defconf);
+		//	echo("<br><br><br>");
+		//	die();
+
+		foreach ($defconf as $cv)
+		{
+			//echo("cv: ");
+			//print_r($cv);
+			//echo("<br>");
+
+			$key = $cv->key;
+			$value = $cv->value;
+
+			// echo($key.": ".$value."<br>");
+			echo("------ $key<br>");
+
+			$sql = "INSERT INTO `zcm_pluginconfigitem` ( `config`, `key`, `value` ) values ($pcg, '".
+				Zymurgy::$db->escape_string($key)."', '".
+				Zymurgy::$db->escape_string($value)."')";
+			$ri = Zymurgy::$db->query($sql);
+			if (!$ri)
+				die("Error adding plugin config: ".Zymurgy::$db->error()."<br>$sql");
+
+			// echo(htmlentities($sql)."<br>");
+		}
+
+		// die();
+
+		echo("---- Initializing plugin<br>");
+
+		$plugin->Initialize();
+	}
+
+	function DisplayPluginList($ri, $action, $error = "")
 	{
 		global $breadcrumbTrail;
 		include("header.php");
@@ -179,6 +323,22 @@
 ?>
 	</table>
 <?
+		if($action == "addlist")
+		{
+?>
+			<h3>Add Plugin</h3>
+
+			<?= strlen($error) > 0 ? "<p>".$error."</p>" : "" ?>
+
+			<form name="frm" action="pluginlist.php" method="POST" enctype="multipart/form-data">
+				<input type="hidden" name="action" value="act_add">
+				<p>File: <input type="file" name="file"></p>
+
+				<p><input type="submit" value="Add Plugin"></p>
+			</form>
+<?
+		}
+
 		include("footer.php");
 	}
 
