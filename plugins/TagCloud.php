df277505c7e7ac13591a10d27666001961fd31dc
cvs <devnull@localhost>
2010-02-18 17:45:59 +0000

    Fixed default filter on tag cloud instances that broke when the parameter building code was made conditional on there actually being a default filter in place.

--- plugins/TagCloud.php
+++ plugins/TagCloud.php
@@ -352,6 +352,7 @@ class PIW_CloudTagCloud extends ZIW_Base
 				$startTags[$cntr] = "s".$cntr."=".$startTags[$cntr];
 			}
 
+			$startTagsParam = implode("&", $startTags);
 			$startTagsParam = "&".$startTagsParam;
 		}
 
