3f9ef44862d4c12d86194e96fe5f120a49569362
cvs <devnull@localhost>
2010-04-07 19:35:18 +0000

    Added PHPDoc.

--- include/acremote.php
+++ include/acremote.php
@@ -1,37 +1,41 @@
 <?
-//Get query parameters.  Barf if they're nfg.
-$table = $_GET['t'];
-$idcolumn = $_GET['i'];
-$column = $_GET['c'];
-$query = $_GET['q'];
+	/**
+	 * Retrieve data from a remote datasource.
+	 */
 
-require_once('../cmo.php');
-require_once("../ZymurgyAuth.php");
-$zauth = new ZymurgyAuth();
-$zauth->Authenticate("/zymurgy/login.php");
+	//Get query parameters.  Barf if they're nfg.
+	$table = $_GET['t'];
+	$idcolumn = $_GET['i'];
+	$column = $_GET['c'];
+	$query = $_GET['q'];
 
-$oktables = explode(',',array_key_exists('RemoteTables',Zymurgy::$config) ? Zymurgy::$config['RemoteTables'] : '');
+	require_once('../cmo.php');
+	require_once("../ZymurgyAuth.php");
+	$zauth = new ZymurgyAuth();
+	$zauth->Authenticate("/zymurgy/login.php");
 
-if (array_search($table,$oktables) === false)
-{
-	die ('["Add '.$table.' to RemoteTables in config.php."]');
-}
+	$oktables = explode(',',array_key_exists('RemoteTables',Zymurgy::$config) ? Zymurgy::$config['RemoteTables'] : '');
 
-//Store possible answers in $r
-$response = Zymurgy::memberremotelookup($table,$column,$query);
-foreach ($response as $key=>$value)
-{
-	$r[] = '{"id": "'.$key.'", "value": "'.$value."\"}";
-}
-if ($r)
-{
-	echo "{ 
-	    \"found\": \"".count($r)."\", 
-	    \"results\": 
-	        [";
-	echo implode(",\r\n",$r);
-	echo "]}";
-}
-else 
-	echo '{}';
+	if (array_search($table,$oktables) === false)
+	{
+		die ('["Add '.$table.' to RemoteTables in config.php."]');
+	}
+
+	//Store possible answers in $r
+	$response = Zymurgy::memberremotelookup($table,$column,$query);
+	foreach ($response as $key=>$value)
+	{
+		$r[] = '{"id": "'.$key.'", "value": "'.$value."\"}";
+	}
+	if ($r)
+	{
+		echo "{
+		    \"found\": \"".count($r)."\",
+		    \"results\":
+		        [";
+		echo implode(",\r\n",$r);
+		echo "]}";
+	}
+	else
+		echo '{}';
 ?>
